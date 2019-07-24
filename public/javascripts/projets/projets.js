99919bf464992a07faa74088c8d6dc9202687515
fviaud <frederic.viaud@hotmail.fr>
2019-07-24 10:59:27 +0200

    commit du mercredi

--- public/javascripts/projets/projets.js
+++ public/javascripts/projets/projets.js
@@ -4,26 +4,26 @@ window.addEventListener("DOMContentLoaded", () => {
 
 function bindObjet() {
   const elements = document.querySelectorAll(".reservations");
-  const reservationsContainer = document.querySelector("#data");
+  const dataContainer = document.querySelector("#data");
   elements.forEach(e => {
     e.addEventListener("click", $event => {
       const projetId = $event.target.getAttribute("projetId");
-      reservationsContainer.innerHTML = "Actualisation en cours ";
+      dataContainer.innerHTML = "Actualisation en cours ";
       axios
         .get("/reservations/" + projetId)
         .then(function(response) {
-          reservationsContainer.innerHTML = response.data;
+          dataContainer.innerHTML = response.data;
         })
         .then(function(response) {
           document
             .querySelector("#BtNewReservation")
             .addEventListener("click", $event => {
-              reservationsContainer.innerHTML = "Actualisation en cours";
+              dataContainer.innerHTML = "Actualisation en cours";
               const projetId = $event.target.getAttribute("projetId");
               axios
                 .get("/reservations/new/" + projetId)
                 .then(function(response) {
-                  reservationsContainer.innerHTML = response.data;
+                  dataContainer.innerHTML = response.data;
                 })
                 .catch(function(err) {
                   console.log(err);
@@ -37,11 +37,11 @@ function bindObjet() {
   });
 
   document.querySelector("#BtNewProjet").addEventListener("click", $event => {
-    reservationsContainer.innerHTML = "Actualisation en cours";
+    dataContainer.innerHTML = "Actualisation en cours";
     axios
       .get("/projets/new/")
       .then(function(response) {
-        reservationsContainer.innerHTML = response.data;
+        dataContainer.innerHTML = response.data;
       })
       .catch(function(err) {
         console.log(err);
@@ -52,9 +52,9 @@ function bindObjet() {
   projets.forEach(e => {
     e.addEventListener("click", $event => {
       const projetId = $event.target.getAttribute("projetId");
-      reservationsContainer.innerHTML = "Actualisation en cours ";
+      dataContainer.innerHTML = "Actualisation en cours ";
       axios.get("/projets/edit/" + projetId).then(function(response) {
-        reservationsContainer.innerHTML = response.data;
+        dataContainer.innerHTML = response.data;
       });
     });
   });
